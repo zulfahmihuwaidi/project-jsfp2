d4830f09901d8c040f824bb0d5f717a6c069d828
zulfahmihuwaidi <muhammad.huwaidi@students.amikom.ac.id>
2018-12-11 11:16:39 +0700

    Project baru Calculator

---
+++ src/lab/amikom/Calculator.java
@@ -0,0 +1,16 @@
+package lab.amikom;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 5;
+		
+		int add = a + b;
+		System.out.println("Hasilnya = "+ add);
+
+	}
+
+}
