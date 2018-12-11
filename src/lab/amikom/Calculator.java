a7fef986826d360643fb7c02e984a98e3071279e
zulfahmihuwaidi <muhammad.huwaidi@students.amikom.ac.id>
2018-12-11 12:05:14 +0700

    Perbaikan Fix

--- src/lab/amikom/Calculator.java
+++ src/lab/amikom/Calculator.java
@@ -9,14 +9,13 @@ public class Calculator {
 		int b = 5;
 		
 		int add = a + b;
-<<<<<<< HEAD
+
 		System.out.println("Hasilnya = "+ add);
 		
 		int sup = a * b;
 		System.out.println("Hasil Perkalian = "+sup);
-=======
+
 		System.out.println("Hasil Jumlah = "+add);
->>>>>>> 327ab7fc42d4ed209d2edf1f9fcee5a33ac5c8b5
 
 		int sub = a - b;
 		System.out.println("Hasil Kurang = "+sub);
